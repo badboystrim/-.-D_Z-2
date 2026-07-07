7cba1fe24ae06f319dddd55e0ab756cbd35a9829
GIRA <ilnurgaliullin931@gmail.com>
2026-07-07 12:12:33 +0300

    dz_14\2 готово| 50\50 осталась проверка.

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,53 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    private final Product[] products = new Product[5];
+    private int count = 0;
+
+    public void addProduct(Product product) {
+        if (count < products.length) {
+            products[count] = product;
+            count++;
+        } else {
+            System.out.println("Невозможно добавить продукт");
+        }
+    }
+
+    public int getTotalCost() {
+        int totalCost = 0;
+        for (int i = 0; i < count; i++) {
+            totalCost += products[i].getPrice();
+        }
+        return totalCost;
+    }
+
+    public void printBasket() {
+        if (count == 0) {
+            System.out.println("в корзине пусто");
+            return;
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(products[i].getTitle() + ": " + products[i].getPrice());
+        }
+        System.out.println("Итого: " + getTotalCost());
+    }
+
+    public boolean hasProduct(String title) {
+        for (int i = 0; i < count; i++) {
+            if (products[i].getTitle().equals(title)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clearBasket() {
+        for (int i = 0; i < count; i++) {
+            products[i] = null;
+        }
+        count = 0;
+    }
+}
